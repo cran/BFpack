f09b4dfddd32f9d96997d685084b4f3b4adaefb7
Joris Mulder <j.mulder3@tilburguniversity.edu>
2026-04-13 21:40:02 +0000

    version 1.6.0

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,23 @@
 .onAttach <- function(libname, pkgname) {
-  version <- read.dcf(
-    file = system.file("DESCRIPTION", package = pkgname),
-    fields = "Version"
+  packageStartupMessage(
+    sprintf("BFpack (%s) loaded", utils::packageVersion("BFpack"))
   )
   packageStartupMessage(
-    "\n",
-    "This is ", paste(pkgname, version),".", "\n",
-    "BFpack is free software. Please report any bugs."
+    "Citation: https://doi.org/10.18637/jss.v100.i18"
   )
 }
 
+# .onAttach <- function(libname, pkgname) {
+#   version <- read.dcf(
+#     file = system.file("DESCRIPTION", package = pkgname),
+#     fields = "Version"
+#   )
+#   packageStartupMessage(
+#     "\n",
+#     "This is ", paste(pkgname, version),".", "\n",
+#     "BFpack is free software. Please report any bugs."
+#   )
+# }
 
 # .onAttach <- function(libname, pkgname) {
 #   packageStartupMessage("test")
